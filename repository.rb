e5a3ede30b3906c4b07418cb5a8ab8193c3a3b9a
William Carey <willtcarey@gmail.com>
2023-03-06 12:41:54 -0600

    Support filtering bookmarks by tags

--- repository.rb
+++ repository.rb
@@ -15,7 +15,8 @@ class Repository
     results.map { |r| r["name"] }
   end
 
-  def list_bookmarks(query, tagged_with: nil)
-    @db.sql("SELECT * FROM bookmarks WHERE url LIKE '%#{query}%' LIMIT 10").map { |r| r["url"] }
+  def list_bookmarks(query, tagged_with:)
+    tag_filter = "SELECT bookmark_id FROM taggings INNER JOIN tags ON taggings.tag_id = tags.id WHERE tags.name = '#{tagged_with}'"
+    @db.sql("SELECT * FROM bookmarks WHERE url LIKE '%#{query}%' AND id IN (#{tag_filter}) LIMIT 10").map { |r| r["url"] }
   end
 end
